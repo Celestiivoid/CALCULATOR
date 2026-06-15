634453277536c6a3d122ec96a11fb1630994a5e2
Joshua Bongcayao <Joshuaesin270@gmail.com>
2026-06-16 00:46:05 +0800

    smol chenz

---
+++ CALCULATOR.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class CALCULATOR {
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        double num1, num2, result;
+        char operator;
+
+        System.out.println("Enter the first number: ");
+        num1 = scanner.nextDouble();
+
+        System.out.println("Enter an operator: ");
+        operator = scanner.next().charAt(0);
+
+        System.out.println("Enter the second number: ");
+        num2 = scanner.nextDouble();
+
+        switch(operator) {
+            case '+':
+                result = num1 + num2;
+                System.out.println("Answer: " + result);
+                break;
+            case '-':
+                result = num1 - num2;
+                System.out.println("Answer: " + result);
+                break;
+            case '*':
+                result = num1 * num2;
+                System.out.println("Answer: " + result);
+                break;
+            case '/':
+                if(num2 != 0) {
+                    result = num1 / num2;
+                    System.out.println("Answer: " + result);
+                }
+                else {
+                    System.out.println("Cannot divide by 0.");
+                }
+                break;
+            default:
+                System.out.println("The operator " + operator + " is not valid. Try again!");
+        }
+    }
+}
